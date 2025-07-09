c466fb8cca4d6a4d5778a31a6d78b409ff6e9a74
Fatos Lala <fatos.lala@akshi.gov.al>
2025-07-09 09:28:00 +0200

    Fix: import Pool from pg

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
+const { Pool } = require('pg');
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
   },
-  family: 4 // 💡 kjo është shtesa që zgjidh problemin me IPv6
 });
+
+module.exports = pool;
