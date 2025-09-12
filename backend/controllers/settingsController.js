df503e136ea82da8c8502694538f904908bcf487
Fatos Lala <fatos.lala@akshi.gov.al>
2025-09-12 12:14:05 +0200

    Fix database import path in settingsController and systemController

--- backend/controllers/settingsController.js
+++ backend/controllers/settingsController.js
@@ -1,4 +1,4 @@
-const pool = require('../config/database');
+const { pool } = require('../db');
 
 // Merr të gjitha system settings
 exports.getSystemSettings = async (req, res) => {
--- backend/controllers/systemController.js
+++ backend/controllers/systemController.js
@@ -1,4 +1,4 @@
-const pool = require('../config/database');
+const { pool } = require('../db');
 
 // Restart system
 exports.restartSystem = async (req, res) => {
